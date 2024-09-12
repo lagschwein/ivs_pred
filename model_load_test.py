aaf591af0be1627b3b245b29711189dafe8086f6
Avinash Malik <avinash.malik@auckland.ac.nz>
2024-09-12 15:31:55 +1200

    Added the best model and test for a model.
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- model_load_test.py
+++ model_load_test.py
@@ -3,46 +3,46 @@
 import keras
 import pred
 import numpy as np
-from keras.preprocessing.image import load_img, img_to_array
 import matplotlib.pyplot as plt
 import glob
 
 
 def date_to_num(date, dd='./figs'):
-    ff = sorted(glob.glob(dd+'/*.png'))
+    ff = sorted(glob.glob(dd+'/*.npy'))
     count = 0
     for i in ff:
-        if i.split('/')[-1].split('_')[0] == date:
+        if i.split('/')[-1].split('.')[0] == date:
             break
         count += 1
     return count
 
 
 def num_to_date(num, dd='./figs'):
-    ff = sorted(glob.glob(dd+'/*.png'))
-    return ff[num].split('/')[-1].split('_')[0]
+    ff = sorted(glob.glob(dd+'/*.npy'))
+    return ff[num].split('/')[-1].split('.')[0]
 
 
-def load_image(num, dd='./figs', RESIZE_FACTOR=8):
-    ff = sorted(glob.glob(dd+'/*.png'))
-    img = load_img(ff[num])
-    w, h = img.size
-    img = img.resize((w//RESIZE_FACTOR, h//RESIZE_FACTOR))
-    img_gray = img.convert('L')
-    img_array = img_to_array(img_gray)
-    return img_array
+def load_image(num, dd='./figs'):
+    ff = sorted(glob.glob(dd+'/*.npy'))
+    img = np.load(ff[num])
+    return img
 
 
 def main():
-    START = date_to_num('20230221')
+    # XXX: Important date: 20201014
+
+    START = date_to_num('20201014')
     print("START:", START)
-    NIMAGES = 20
+    NIMAGES = 30
 
     # XXX: Load the test dataset
     pred.TSTEPS = 5
-    valX, valY, Ydates = pred.load_image_for_keras(START=START,
-                                                   NUM_IMAGES=NIMAGES,
-                                                   TSTEP=pred.TSTEPS)
+    bs = 5
+    nf = 64
+
+    valX, valY, Ydates = pred.load_data_for_keras(START=START,
+                                                  NUM_IMAGES=NIMAGES,
+                                                  TSTEP=pred.TSTEPS)
     print(valX.shape, valY.shape)
     valX = valX.reshape(valX.shape[0]//pred.TSTEPS, pred.TSTEPS,
                         *valX.shape[1:])
@@ -50,38 +50,77 @@ def main():
 
     # XXX: Load the model
     m1 = keras.saving.load_model(
-        './modelcr_bs_2_ts_%s_filters_64.keras' % (pred.TSTEPS))
+        './modelcr_bs_%s_ts_%s_filters_%s.keras' % (bs, pred.TSTEPS, nf))
     print(m1.summary())
+
+    # XXX: The moneyness
+    MS = np.arange(pred.LM, pred.UM+pred.MSTEP, pred.MSTEP)
+    # XXX: The term structure
+    TS = np.array([i/pred.DAYS
+                   for i in
+                   range(pred.LT, pred.UT+pred.TSTEP, pred.TSTEP)])
 
     # XXX: Make a prediction for 10 images
     out = m1(valX, training=False).numpy()
-    print(out.shape)
+
     for y, yd, yp in zip(valY, Ydates, out):
-        y *= 255
-        yp *= 255
-        # x *= 255
-        y = np.uint8(y)
-        yp = np.uint8(yp)
-        # x = np.uint8(x)
-        # xa.title.set_text('Input')
-        # xa.imshow(x)
+        y *= 100
+        yp *= 100
         ynum = date_to_num(yd)
         pyd = num_to_date(ynum-1)
-        print(int(yd), int(pyd))
         if (int(yd) - int(pyd)) == 1:
             # XXX: Then we can get this figure
+            fig, axs = plt.subplots(1, 3,
+                                    subplot_kw=dict(projection='3d'))
+            axs[0].title.set_text('Truth: ' + yd)
+            # XXX: Make the y dataframe
+            ydf = list()
+            for cm, m in enumerate(MS):
+                for ct, t in enumerate(TS):
+                    ydf.append([m, t, y[cm, ct]])
+            ydf = np.array(ydf)
+            axs[0].plot_trisurf(ydf[:, 0], ydf[:, 1], ydf[:, 2],
+                                cmap='afmhot', linewidth=0.2,
+                                antialiased=True)
+            axs[0].set_xlabel('Moneyness')
+            axs[0].set_ylabel('Term structure')
+            axs[0].set_zlabel('Vol %')
+            # axs[0, 0].imshow(np.transpose(y), cmap='hot')
+            axs[1].title.set_text('Predicted: ' + yd)
+            ypdf = list()
+            for cm, m in enumerate(MS):
+                for ct, t in enumerate(TS):
+                    ypdf.append([m, t, yp[cm, ct]])
+            ypdf = np.array(ypdf)
+            axs[1].plot_trisurf(ypdf[:, 0], ypdf[:, 1], ypdf[:, 2],
+                                cmap='afmhot', linewidth=0.2,
+                                antialiased=True)
+            axs[1].set_xlabel('Moneyness')
+            axs[1].set_ylabel('Term structure')
+            axs[1].set_zlabel('Vol %')
+            # axs[0, 1].imshow(np.transpose(yp), cmap='hot')
+
+            # XXX: Previous day' volatility
             ximg = load_image(ynum-1)
-            fig, axs = plt.subplots(2, 2, sharex=True)
-            axs[0, 0].title.set_text('Ground truth date: ' + yd)
-            axs[0, 0].imshow(y, cmap='hot')
-            axs[0, 1].title.set_text('Predicted date: ' + yd)
-            axs[0, 1].imshow(yp, cmap='hot')
-            dimgt = y - ximg
-            axs[1, 0].imshow(dimgt, cmap='PuBu')
-            axs[1, 0].title.set_text('True diff : %s_%s' % (yd, pyd))
-            dimg = yp - ximg
-            axs[1, 1].imshow(dimg, cmap='PuBu')
-            axs[1, 1].title.set_text('diff dates :%s-%s' % (yd, pyd))
+            ximg *= 100
+            xdf = list()
+            for cm, m in enumerate(MS):
+                for ct, t in enumerate(TS):
+                    xdf.append([m, t, ximg[cm, ct]])
+            xdf = np.array(xdf)
+            axs[2].plot_trisurf(xdf[:, 0], xdf[:, 1], xdf[:, 2],
+                                cmap='afmhot', linewidth=0.2,
+                                antialiased=True)
+            axs[2].set_xlabel('Moneyness')
+            axs[2].set_ylabel('Term structure')
+            axs[2].set_zlabel('Vol %')
+            axs[2].title.set_text(pyd)
+            # dimgt = y - ximg
+            # axs[1, 0].imshow(np.transpose(dimgt), cmap='hot')
+            # axs[1, 0].title.set_text('True diff : %s_%s' % (yd, pyd))
+            # dimg = yp - ximg
+            # axs[1, 1].imshow(np.transpose(dimg), cmap='hot')
+            # axs[1, 1].title.set_text('diff dates :%s-%s' % (yd, pyd))
             plt.show()
             plt.close(fig)
 
